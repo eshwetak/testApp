787524fcb2b3721bc9a652c3bdab480ed30a1a5a
shweta <eshwetak@gmail.com>
2018-07-01 19:38:09 +0530

    changed port

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ const app = express();
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './', 'index.html')));
 
-app.listen(9900, () => console.log('Example app listening on port 9900!'));
+app.listen(9090, () => console.log('Example app listening on port 9090!'));
